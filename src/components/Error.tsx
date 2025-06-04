92c2ce0cce4d9f4e0acce9376953da7024ce4450
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-06-05 00:45:36 +0200

    Creando un componente para mensajes de error

---
+++ src/components/Error.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type ErrorProps = {
+  children: React.ReactNode;
+};
+
+export const Error = ({ children }: ErrorProps) => {
+  return (
+    <p className="text-center my-4 bg-red-600 text-white font-bold p-3 uppercase text-sm">
+      {children}
+    </p>
+  );
+};
