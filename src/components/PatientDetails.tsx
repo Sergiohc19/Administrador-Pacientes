37509a1a13819150acd043840c5596c6e3712557
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-06-08 19:45:53 +0200

    Mostrando la información del Paciente

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,13 +1,21 @@
-import type { Patient } from "../types"
+import type { Patient } from "../types";
+import { PatientDetailItem } from "./PatientDetailItem";
 
 type PatientDetailsProps = {
-    patient: Patient
-}
+  patient: Patient;
+};
 
-export const PatientDetails = ({patient} : PatientDetailsProps) => {
+export const PatientDetails = ({ patient }: PatientDetailsProps) => {
   return (
-    <div>
-        {patient.name}
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+      <PatientDetailItem label="ID" data={patient.id} />
+      <PatientDetailItem label="Nombre" data={patient.name} />
+      <PatientDetailItem label="Propietario" data={patient.caretaker} />
+      <PatientDetailItem label="Email" data={patient.email} />
+      <PatientDetailItem label="Fecha Alta" data={patient.date.toString()} />
+      <PatientDetailItem label="Sintomas" data={patient.symptoms} />
+
+   
     </div>
-  )
-}
+  );
+};
