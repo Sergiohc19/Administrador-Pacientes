18abd789e23943ca37e028d1388df9e7231f49b6
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-06-08 19:03:35 +0200

    Escribiendo en el Estado de Zustand

--- src/components/PatientsList.tsx
+++ src/components/PatientsList.tsx
@@ -1,6 +1,14 @@
 
+import { usePatientStore } from "../store/store"
+
+
+
 
 export const PatientsList = () => {
+const patients = usePatientStore(state => state.patients)
+
+console.log(patients)
+
   return (
     <div>PatientsList</div>
   )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { v4 as uuidv4 } from "uuid"
 import type { DraftPatient, Patient } from '../types';
 
 type PatientState = {
@@ -6,11 +7,21 @@ type PatientState = {
     addPatient: (data: DraftPatient) => void;
 }
 
+const createPatient = (patient: DraftPatient): Patient => {
 
+    return {
+        ...patient,
+        id: uuidv4()
+    }
 
-export const usePatientStore = create<PatientState>(() => ({
+};
+
+export const usePatientStore = create<PatientState>((set) => ({
     patients: [],
     addPatient: (data) => {
-        console.log(data);
+        const newPatient = createPatient(data);
+        set((state) => ({
+            patients: [...state.patients, newPatient]
+        }))
     }
 }))
